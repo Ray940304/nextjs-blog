4b28ba873cf32c44fa2561a753be813496924ceb
Ray <a5512150@gmail.com>
2023-02-17 12:00:50 +0800

    set up Next.js with TypeScript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,34 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.scss';
-// import { getSortedPostsData, loadPosts } from '../lib/posts';
 import { getSortedPostsData } from '../lib/posts';
 import Link from 'next/link';
 import Date from '../components/date';
+import { GetStaticProps } from 'next';
 
-export async function getStaticProps() {
-	const allPostsData = getSortedPostsData();
-	// const posts = await loadPosts();
-	return {
-		props: {
-			allPostsData,
-			// posts,
-		},
-	};
-}
-
-// export default function Home({ allPostsData, posts }) {
-export default function Home({ allPostsData }) {
+export default function Home({
+	allPostsData,
+}: {
+	allPostsData: {
+		date: string;
+		title: string;
+		id: string;
+	}[];
+}) {
 	return (
 		<Layout home>
 			<Head>
 				<title>{siteTitle}</title>
 			</Head>
 			<section className={utilStyles.headingMd}>
-				<p>Beginners with React.</p>
+				<p>[Your Self Introduction]</p>
 				<p>
 					(This is a sample website - you’ll be building a site like
-					this on{' '}
+					this in{' '}
 					<a href="https://nextjs.org/learn">our Next.js tutorial</a>
 					.)
 				</p>
 			</section>
-
 			<section
 				className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}
 			>
@@ -50,10 +45,15 @@ export default function Home({ allPostsData }) {
 					))}
 				</ul>
 			</section>
-
-			{/* <section>
-				<p>{JSON.stringify(posts)}</p>
-			</section> */}
 		</Layout>
 	);
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+	const allPostsData = getSortedPostsData();
+	return {
+		props: {
+			allPostsData,
+		},
+	};
+};
